d6e4bddd7e565f393663fdfa86b8afe2d16f9ec0
Drake Emko <demko@revionics.com>
2014-09-13 10:43:23 -0600

    fixing broken trading pairs

--- appengine/main.py
+++ appengine/main.py
@@ -19,7 +19,8 @@ TRADING_PAIR_URL = 'http://www.cryptocoincharts.info/v2/api/tradingPair/'
 TRADING_PAIR_URL_BTC_BACKUP="https://api.mintpal.com/v1/market/stats/DOGE/"
 TRADING_PAIR_URL_USD_BACKUP = 'https://coinbase.com/api/v1/prices/buy' 
 # TRADING_PAIR_URL_FIAT_BACKUP = 'http://api.bitcoincharts.com/v1/markets.json'
-BTCAVERAGE_URL = 'https://api.bitcoinaverage.com/ticker/' # used for BTC / GBP, AUD
+BTCAVERAGE_URL = 'https://api.bitcoinaverage.com/ticker/' # used for BTC / (CNY, GBP, EUR, AUD)
+BTER_LTC_BTC_URL = 'http://data.bter.com/api/1/ticker/ltc_btc'
 
 TIMEOUT_DEADLINE = 12 # seconds
 
@@ -81,7 +82,7 @@ def tradingDOGE(currency='BTC'):
     response.content_type = 'application/json; charset=utf-8'
 
     mReturn = '{}'
-    if (currency not in ['EUR', 'USD', 'GBP', 'AUD']):
+    if (currency not in ['CNY', 'EUR', 'GBP', 'USD', 'LTC', 'AUD']):
         dogeCurrency = json.loads(memcache.get('trading_DOGE_' + currency))
         if (not dogeCurrency):
             logging.warn('No data found in memcache for trading_DOGE_' + currency)
@@ -89,7 +90,8 @@ def tradingDOGE(currency='BTC'):
         else:
             mReturn = dogeCurrency['price']
     else:
-        # For EUR, GBP, and USD We have to convert from DOGE -> BTC -> FIAT
+        # For CNY, EUR, GBP, USD, AUD We have to convert from DOGE -> BTC -> FIAT
+        # UPDATE: now also for LTC
         dogeBtc = json.loads(memcache.get('trading_DOGE_BTC'))
         if (not dogeBtc):
             logging.warn("No data found in memcache for trading_DOGE_BTC")
@@ -111,7 +113,7 @@ def tradingDOGE(currency='BTC'):
     return str(mReturn)
 
 def pullTradingPair(currency1='DOGE', currency2='BTC'):
-    url = BTCAVERAGE_URL + currency2 + '/' if currency2 in ['AUD', 'GBP'] else TRADING_PAIR_URL + currency1 + '_' + currency2
+    url = BTCAVERAGE_URL + currency2 + '/' if currency2 in ['CNY', 'EUR', 'GBP', 'USD', 'AUD'] else TRADING_PAIR_URL + currency1 + '_' + currency2
     data = None
     useBackupUrl = False
 
@@ -125,26 +127,31 @@ def pullTradingPair(currency1='DOGE', currency2='BTC'):
         useBackupUrl = True
 
     if (useBackupUrl):
+        backupUrl = ''
         if (currency1 == 'DOGE' and currency2 == 'BTC'):
             backupUrl = TRADING_PAIR_URL_BTC_BACKUP + currency2
-            logging.warn('Now trying ' + backupUrl)
-            data = urlfetch.fetch(backupUrl, deadline=TIMEOUT_DEADLINE)
+        elif (currency1 == 'BTC' and currency2 == 'LTC'):
+            backupUrl = BTER_LTC_BTC_URL
         elif (currency1 == 'BTC' and currency2 == 'USD'):
             backupUrl = TRADING_PAIR_URL_USD_BACKUP
-            logging.warn('Now trying ' + backupUrl)
-            data = urlfetch.fetch(backupUrl, deadline=TIMEOUT_DEADLINE)
         else:
             logger.error('Cannot get trading pair for ' + currency1 + ' / ' + currency2)
             return
 
+        logging.warn('Now trying ' + backupUrl)
+        data = urlfetch.fetch(backupUrl, deadline=TIMEOUT_DEADLINE)
+
     dataDict = json.loads(data.content)
-    if (currency1 == 'BTC' and currency2 in ['AUD', 'GBP']):
+    if (currency1 == 'BTC' and currency2 in ['CNY', 'EUR', 'GBP', 'USD', 'AUD']):
         # standardize format of exchange rate data from different APIs (we will use 'price' as a key)
         dataDict['price'] = dataDict['last'] 
 
     if (useBackupUrl):
         if (currency1 == 'DOGE' and currency2 == 'BTC'):
             dataDict = {'price': dataDict[0]['last_price']}
+        elif (currency1 == 'BTC' and currency2 == 'LTC'):
+            price = Decimal(1) / Decimal(dataDict['last'])
+            dataDict = {'price': price}
         elif (currency1 == 'BTC' and currency2 == 'USD'):
             if (dataDict['subtotal']['currency'] == 'USD'):
                 dataDict = {'price': dataDict['subtotal']['amount']}
@@ -160,11 +167,11 @@ def pullTradingPair(currency1='DOGE', currency2='BTC'):
 @bottle.route('/tasks/pull-cryptocoincharts-data')
 def pullCryptocoinchartsData():
     pullTradingPair('DOGE', 'BTC')
+    pullTradingPair('BTC', 'CNY')
     pullTradingPair('BTC', 'USD')
-    pullTradingPair('DOGE', 'LTC')
-    pullTradingPair('DOGE', 'CNY')
     pullTradingPair('BTC', 'EUR')
     pullTradingPair('BTC', 'GBP')
+    pullTradingPair('BTC', 'LTC')
     #pullTradingPair('BTC', 'AUD')
     return "Done"
 
